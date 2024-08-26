e98b18736e8fe53bb32780353daaeff9aa72d671
Jostar <shenjunjiekoda@foxmail.com>
2024-08-26 13:52:48 +0800

    [nfc] adjust testcase

--- test/testcase/if-else.cpp
+++ test/testcase/if-else.cpp
@@ -5,10 +5,16 @@ void knight_dump_zval(int);
 
 int foo(int x) {
     int a = 0;
+    knight_dump_zval(a);
+    // warning:-1:22:-1:22: 0 [debug-inspection]
     if (x) {
         a = 1;
+        knight_dump_zval(a);
+        // warning:-1:26:-1:26: 1 [debug-inspection]
     } else {
         a = -1;
+        knight_dump_zval(a);
+        // warning:-1:26:-1:26: -1 [debug-inspection]
     }
     knight_dump_zval(a);
     // warning:-1:22:-1:22: [-1, 1] [debug-inspection]
