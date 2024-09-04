96a2744caa17dfd606b767f13c17d7c3b0f53d74
Jostar <shenjunjiekoda@foxmail.com>
2024-09-05 00:23:50 +0800

    [feat] add windows workflow

--- cmake/addClang.cmake
+++ cmake/addClang.cmake
@@ -36,11 +36,11 @@ if(NOT CLANG_TOOLS_BINARY_DIR)
   set(CLANG_TOOLS_BINARY_DIR "${LLVM_TOOLS_BINARY_DIR}")
 endif()
 
+message(STATUS "Try find Clang executable: ${CLANG_TOOLS_BINARY_DIR}/clang")
+
 find_file(CLANG_EXECUTABLE NAMES clang PATHS ${CLANG_TOOLS_BINARY_DIR} NO_DEFAULT_PATH)
 
-if(NOT CLANG_EXECUTABLE)
-  message(FATAL_ERROR "Clang executable not found.")
-else()
+if(CLANG_EXECUTABLE)
   message(STATUS "Found Clang executable: ${CLANG_EXECUTABLE}")
   execute_process(COMMAND ${CLANG_EXECUTABLE} --version
     OUTPUT_VARIABLE CLANG_VERSION_STRING
@@ -48,22 +48,25 @@ else()
     OUTPUT_STRIP_TRAILING_WHITESPACE)
   string(REGEX MATCH "version ([0-9]+\\.[0-9]+\\.[0-9]+)" CLANG_VERSION_MATCH ${CLANG_VERSION_STRING})
   set(CLANG_VERSION ${CMAKE_MATCH_1})
+
+  message(STATUS "Found Clang ${CLANG_VERSION}")
+  message(STATUS "Found Clang include directory: ${CLANG_INCLUDE_DIRS}")
+  message(STATUS "Found Clang library directory: ${CLANG_LIBRARY_DIRS}")
+  message(STATUS "Found Clang binary directory: ${CLANG_TOOLS_BINARY_DIR}")
+
+  string(REGEX MATCH "^([0-9]+\\.[0-9]+)" CLANG_VERSION_MAJOR ${CLANG_VERSION})
+  string(REGEX MATCH "^([0-9]+)" CLANG_VERSION_MAJOR ${CLANG_VERSION_MAJOR})
+  string(REGEX MATCH "^([0-9]+\\.[0-9]+)" LLVM_VERSION_MAJOR ${LLVM_VERSION})
+  string(REGEX MATCH "^([0-9]+)" LLVM_VERSION_MAJOR ${LLVM_VERSION_MAJOR})
+
+  if(NOT CLANG_VERSION_MAJOR STREQUAL LLVM_VERSION_MAJOR)
+    message(FATAL_ERROR "Clang version ${CLANG_VERSION} does not match LLVM version ${LLVM_VERSION}")
+  endif()
+
+  message(STATUS "Using Clang version: ${CLANG_VERSION_MAJOR}")
+else()
+  message(STATUS "Using unknown Clang version")
 endif()
 
-message(STATUS "Found Clang ${CLANG_VERSION}")
-message(STATUS "Found Clang include directory: ${CLANG_INCLUDE_DIRS}")
-message(STATUS "Found Clang library directory: ${CLANG_LIBRARY_DIRS}")
-message(STATUS "Found Clang binary directory: ${CLANG_TOOLS_BINARY_DIR}")
-
-string(REGEX MATCH "^([0-9]+\\.[0-9]+)" CLANG_VERSION_MAJOR ${CLANG_VERSION})
-string(REGEX MATCH "^([0-9]+)" CLANG_VERSION_MAJOR ${CLANG_VERSION_MAJOR})
-string(REGEX MATCH "^([0-9]+\\.[0-9]+)" LLVM_VERSION_MAJOR ${LLVM_VERSION})
-string(REGEX MATCH "^([0-9]+)" LLVM_VERSION_MAJOR ${LLVM_VERSION_MAJOR})
-
-if(NOT CLANG_VERSION_MAJOR STREQUAL LLVM_VERSION_MAJOR)
-  message(FATAL_ERROR "Clang version ${CLANG_VERSION} does not match LLVM version ${LLVM_VERSION}")
-endif()
-
-message(STATUS "Using Clang version: ${CLANG_VERSION_MAJOR}")
 message(STATUS "Enabled LLVM LIBS: ${LLVM_LIBS}")
 message(STATUS "Enabled Clang LIBS: ${CLANG_LIBS}")
